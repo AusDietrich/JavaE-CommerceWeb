e522715287de0cf691b4412164b232f9c1a3858f
AuDietrich <33529775+ASP-React-Database@users.noreply.github.com>
2020-08-11 16:35:44 -0700

    added js shopping cart

--- src/main/java/com/Audie/JavaECommWeb/api/EcommWebController.java
+++ src/main/java/com/Audie/JavaECommWeb/api/EcommWebController.java
@@ -1,11 +1,15 @@
 package com.Audie.JavaECommWeb.api;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.Audie.JavaECommWeb.ApiCaller.ApiCaller;
+import com.Audie.JavaECommWeb.model.ProductList;
 import com.Audie.JavaECommWeb.svc.EcommWebSvc;
 
 @RestController
@@ -22,8 +26,12 @@ public class EcommWebController {
 	    return modelAndView;
 	}
 	
-	@GetMapping("/co")
-	public void colores() {
-		svc.productListCaller();
+	@GetMapping("/products")
+	public ModelAndView colores(Model model) {
+		List<ProductList> prodList = svc.productListCaller();
+		model.addAttribute("prodList", prodList);
+		ModelAndView modelAndView = new ModelAndView();
+	    modelAndView.setViewName("products");
+	    return modelAndView;
 	}
 }
